e3115a72c373f034cc86abf667de6d88ca6d1735
Sean Oliver <soliver87@gmail.com>
2011-08-28 23:30:26 -0700

    user signup complete

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Users" do
+  
+  describe "signup" do
+    
+    describe "failure" do
+      
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          fill_in :user_name,                   :with => ""
+          fill_in :user_email,                  :with => ""
+          fill_in :user_password,               :with => ""
+          fill_in :user_password_confirmation,  :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(User, :count)
+      end
+      
+      describe "success" do
+        
+        it "should make a new user" do
+          lambda do
+            visit signup_path
+            fill_in :user_name,                   :with => "Example User"
+            fill_in :user_email,                  :with => "example@user.com"
+            fill_in :user_password,               :with => "foobar"
+            fill_in :user_password_confirmation,  :with => "foobar"
+            click_button
+            response.should have_selector("div.flash.success", :content => "Welcome")
+            response.should render_template('users/show')
+          end.should change(User, :count).by(1)
+        end
+      end
+    end
+  end
+end
